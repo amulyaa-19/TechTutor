4f97411696608397d1d6ffe79ba876408f5ed205
Amulya Srivastava <srivastava.amulya19@gmail.com>
2024-09-23 12:30:18 +0530

    made mongoose connect async

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use("/api/v1/user" , userRouter);
 app.use("/api/v1/admin" , adminRouter);
 app.use("/api/v1/course", courseRouter); 
 
-
-
+async function main(){
+await mongoose.connect("mongodb+srv://srivastavaamulya19:Amulya19@cluster0.dukem.mongodb.net/techTutor")
 app.listen(4001);
+}
+
+main();
