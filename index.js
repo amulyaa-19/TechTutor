3f4e23ddc6027615369b2f68c29c4542480f7bfd
Amulya Srivastava <srivastava.amulya19@gmail.com>
2024-09-23 12:42:16 +0530

    Made use od the dotenv file to remove connection string

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin")
+
+
+const app = express();
+app.use(express.json());
+
+
+app.use("/api/v1/user" , userRouter);
+app.use("/api/v1/admin" , adminRouter);
+app.use("/api/v1/course", courseRouter); 
+
+app.listen(4001);
+main();
